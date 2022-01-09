bcbcd5b4bfcb954416307d6f29a99a695eb2542c
Hammad-e-Rehman <95103558+Hammad-e-Rehman@users.noreply.github.com>
2022-01-09 21:15:15 +0500

    Added new startup class

---
+++ startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+public class startup
+{
+	
+
+
+		public void Configurationservices(IServiceCollection service)
+		{
+
+
+		}
+
+		public void configure(IApplicationBuilder app,IWebHostEnvironment env)
+		{
+			app.UseRouting();
+			app.UseEndpoints(endpoints =>
+			{
+
+
+
+				endpoints.MapGet("/", async context =>
+				{
+
+                    await context.Response.WriteAsync("hello bookstore");
+				});
+			}
+
+
+			);
+
+		}
+
+
+
+		
+	}
+
+ 
+
